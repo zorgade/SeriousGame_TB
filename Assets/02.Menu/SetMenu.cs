8c464dddc4ef2c3afb50d8b8db7eff657b024dbf
taylan.caloz <uregen.taylan@gmail.com>
2018-04-26 11:28:28 +0200

    Menu
    begin set Menu lang

---
+++ Assets/02.Menu/SetMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetIntro.ChangeLanguage("fr");
+            }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
